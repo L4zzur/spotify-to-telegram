c844660115118f2c1b46f65fe57fa381ce263e18
L4ZZURPC\L4zzur <popovnikita01@gmail.com>
2022-02-23 18:29:16 +0200

    initial

---
+++ config.py
@@ -0,0 +1,10 @@
+# TELEGRAM
+api_id = 0 # paste telegram api_id
+api_hash = 'paste telegram api_hash'
+status = 'paste your stock bio'
+# SPOTIFY
+client_id = 'paste client_id' 
+client_secret = 'paste client_secret'
+redirect_uri = "http://localhost:8888/" # don't touch
+username = 'paste username'
+scope = 'user-read-currently-playing' # don't touch
